010b4bf55ee4586e2b15ab3d0c03d82dd21de37b
Ares <srkanuri@iu.edu>
2016-09-06 11:33:02 -0400

    Finished Lab. Report Pending

--- Lab2/poly/dump.c
+++ Lab2/poly/dump.c
@@ -6,8 +6,9 @@
 void dump_memory(void *p, int len)
 {
   int i;
-  printf("|%-14s|%s|%s|%-6s|%-11s|%-13s|%-13s|\n","address","char","hexCh","short","integer","float","doubleFloat");
+  printf("%-14s  %4s %5s %-6s %+11s%+13s %+13s\n","address","char","hexCh","short","integer","float","doubleFloat");
   for (i = 0; i < len; i++) {
-    printf("%8p %c 0x%02x %+hd %+d %+g %+lg\n", p + i, *(unsigned char *)(p + i), *(unsigned char *)(p + i), *(unsigned short *)(p + i), *(unsigned int *)(p + i), *(float *)(p + i), *(double *)(p + i));
+    char tmp = *(unsigned char *)(p + i);
+    printf("%+14p %4c 0x%02x %+6hd %+11d %+13e %+13e\n", p + i, ((tmp>31 && tmp<128) ? tmp :'?'), *(unsigned char *)(p + i), *(unsigned short *)(p + i), *(unsigned int *)(p + i), *(float *)(p + i), *(double *)(p + i));
   }
 }
--- Lab2/sample.c
+++ Lab2/sample.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main(){
+  char a[100];
+  int i;
+  while(scanf("%s",a)){
+    printf("Value entered is %s\n", a);
+    if(isdigit(a)){
+      int val = strtol(a,&a,10);
+      printf("%d",val);
+    }
+    for(i=0;a[i]!='\0';i++){
+      printf("%c\n",a[i]);
+    }
+  }
+}
+
+/*int main(){
   printf("%d\n",65535.2345);
   printf("%0hd\n", 65535.2345);
   printf("%ld\n", 65535.2345);
@@ -17,4 +32,4 @@ int main(){
   printf("%10s\n", 65535.2345);
   printf("%10.2f\n", 65535.2345);
   printf("%10eg\n", 65535.2345);
-}
+  }*/
